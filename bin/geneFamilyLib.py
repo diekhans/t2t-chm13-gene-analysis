2b3bee6cef4fb2d5a9f8d3c7c6edb7ad70ef6052
Mark Diekhans <markd@ucsc.edu>
2020-09-16 15:22:29 -0700

    made bounds stuff common

--- bin/geneFamilyLib.py
+++ bin/geneFamilyLib.py
@@ -1,4 +1,5 @@
 from pycbio.hgdata.bed import Bed
+from collections import defaultdict, deque
 
 def whackVersion(accid):
     return accid.split('.')[0]
@@ -55,3 +56,59 @@ class GeneBoundsBed(Bed):
     def parse(cls, row, numStdCols=None):
         assert numStdCols == 6
         return Bed.parse(row, numStdCols=6)
+
+
+# code to merge into gene bounds
+def overlaps(b1, b2):
+    return (b1.chrom == b2.chrom) and (b1.strand == b2.strand) and (b1.start < b2.end) and (b1.end > b2.start)
+
+def merge(b1, b2):
+    b = GeneBoundsBed(b1.chrom, min(b1.start, b2.start), max(b1.end, b2.end), b1.name, 0, b1.strand, extraCols=b1.extraCols)
+    b.addGeneIds(b2.geneIds)
+    return b
+
+def mergeOrAdd(outRanges, inBed):
+    for i in range(len(outRanges)):
+        if overlaps(outRanges[i], inBed):
+            outRanges[i] = merge(outRanges[i], inBed)
+            return True
+    outRanges.append(inBed)
+    return False
+
+def clusterPass(inRanges, outRanges):
+    mergedSome = False
+    while len(inRanges) > 0:
+        if mergeOrAdd(outRanges, inRanges.pop()):
+            mergedSome = True
+    return mergedSome
+
+def clusterGeneTrans(transBeds):
+    "cluster transcripts for a gene into overlapping beds"
+    inRanges = deque(transBeds)
+    outRanges = deque()
+    while True:
+        if not clusterPass(inRanges, outRanges):
+            break
+        inRanges, outRanges = outRanges, inRanges
+    assert len(inRanges) == 0
+    return list(outRanges)
+
+def clusterGenes(geneTransBeds):
+    "cluster all genes ranges on a chrom so we can sort"
+    genesRanges = []
+    for transBeds in geneTransBeds.values():
+        genesRanges.extend(clusterGeneTrans(transBeds))
+    return genesRanges
+
+def setLociNumbers(genesRanges):
+    geneIdLociCounts = defaultdict(int)
+    for gr in genesRanges:
+        gr.score = geneIdLociCounts[gr.name]
+        geneIdLociCounts[gr.name] += 1
+
+def buildGeneBounds(geneTransBeds, bedFh):
+    genesRanges = clusterGenes(geneTransBeds)
+    genesRanges.sort(key=lambda b: (b.chrom, b.start, -b.end))
+    setLociNumbers(genesRanges)
+    for geneRange in genesRanges:
+        geneRange.write(bedFh)
