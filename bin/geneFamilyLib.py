9b26513d12f2b2e9186a81a9fd5c44fa4cb7b0cc
Mark Diekhans <markd@ucsc.edu>
2020-09-19 21:53:22 -0700

    work on bounds bed rather than clusters

--- bin/geneFamilyLib.py
+++ bin/geneFamilyLib.py
@@ -1,8 +1,15 @@
 from pycbio.hgdata.bed import Bed
 from collections import defaultdict, deque
+from pycbio.hgdata.bed import BedReader
 
 def whackVersion(accid):
     return accid.split('.')[0]
+
+def splitStrList(v):
+    return v.split(',')
+
+def joinStrList(l):
+    return ",".join(sorted(set(l)))
 
 class GeneBoundsBed(Bed):
     """adds properties that are saved in extraCols record
@@ -10,40 +17,43 @@ class GeneBoundsBed(Bed):
     hgncId - symbol id
     geneIds - database gene ids, there maybe multiple mapping to the same symbol with readthroughs.
     geneType - type of gene
+
     """
 
     def __init__(self, chrom, chromStart, chromEnd, name=None, score=None, strand=None,
                  thickStart=None, thickEnd=None, itemRgb=None, blocks=None, extraCols=None):
         super().__init__(chrom, chromStart, chromEnd, name=name, score=score, strand=strand,
                          thickStart=thickStart, thickEnd=thickEnd, itemRgb=itemRgb, blocks=blocks, extraCols=extraCols)
-        self._geneIdsCache = None
         if self.extraCols is None:
-            self.extraCols = tuple(4 * [""])
+            self.extraCols = 4 * [""]
+        else:
+            self.extraCols = ["" if v == "N/A" else v for v in self.extraCols]
+        if isinstance(self.extraCols, tuple):
+            self.extraCols = list(self.extraCols)
 
     @property
     def geneSym(self):
         return self.extraCols[0]
 
-    @property
-    def hgncId(self, v):
-        self.extraCols[1] = v
+    def setGeneSym(self, v):
+        self.extraCols[0] = v
 
-    def _mkGeneIdsCache(self):
-        self._geneIdsCache = tuple(self.extraCols[2].split(','))
+    @property
+    def hgncId(self):
+        return self.extraCols[1]
+
+    def setHgncId(self, v):
+        self.extraCols[1] = v
 
     @property
     def geneIds(self):
-        if self._geneIdsCache is None:
-            self._mkGeneIdsCache()
-        return self._geneIdsCache
+        return splitStrList(self.extraCols[2])
+
+    def setGeneIds(self, geneIds):
+        self.extraCols[2] = joinStrList(geneIds)
 
     def addGeneIds(self, newIds):
-        if self._geneIdsCache is None:
-            self._mkGeneIdsCache()
-        self._geneIdsCache = tuple(set(self._geneIdsCache + tuple(newIds)))
-        ex = list(self.extraCols)
-        ex[2] = ",".join(sorted(self._geneIdsCache))
-        self.extraCols = tuple(ex)
+        self.setGeneIds(set(self.geneIds) | set(newIds))
 
     def addGeneId(self, newId):
         self.addGeneIds((newId, ))
@@ -52,11 +62,16 @@ class GeneBoundsBed(Bed):
     def geneType(self):
         return self.extraCols[3]
 
+    def setGeneType(self, v):
+        self.extraCols[3] = v
+
     @classmethod
     def parse(cls, row, numStdCols=None):
         assert numStdCols == 6
-        return Bed.parse(row, numStdCols=6)
+        return super().parse(row, numStdCols=6)
 
+def GeneBoundsBedReader(geneBoundsBed):
+    return BedReader(geneBoundsBed, bedClass=GeneBoundsBed, numStdCols=6)
 
 # code to merge into gene bounds
 def overlaps(b1, b2):
